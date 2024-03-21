7c4cbe0742f585bc5a2b60c960d1a3d816d3ca93
andyzheng2019 <54092608+andyzheng2019@users.noreply.github.com>
2024-03-21 10:34:45 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-jvlq58
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/andyzheng2019/angular-ivy-jvlq58)
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSquare as farSquare,
+  faCheckSquare as farCheckSquare,
+  faUser,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faStackOverflow,
+  faGithub,
+  faMedium,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, FontAwesomeModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faSquare,
+      faCheckSquare,
+      farSquare,
+      farCheckSquare,
+      faStackOverflow,
+      faGithub,
+      faMedium,
+      faUser
+    );
+  }
+}
